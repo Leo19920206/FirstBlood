02dbf7904e0c0f1f16c28be0c36428d3b5fc2818
heli <2446060110@qq.com>
2017-07-10 22:29:30 +0800

    修改

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // js goes here
 
-alert("Hello world!😆😆");<<<<<<< HEAD
 alert("Hello world!😆😆");
-=======
+alert("Hello world!😆😆");
 alert("合情合理!");
->>>>>>> 6f312c8008ae208daec84dc8ab024bdd6d38b860
